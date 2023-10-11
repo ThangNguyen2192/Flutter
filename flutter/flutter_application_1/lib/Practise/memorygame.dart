a74736fa94f618e816811355af432cd74224e4bd
ThangNguyen2192 <nguyenthang.effect@gmail.com>
2023-10-11 15:32:25 +0700

    abc

--- flutter/flutter_application_1/lib/Practise/memorygame.dart
+++ flutter/flutter_application_1/lib/Practise/memorygame.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -13,152 +14,142 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
+int count = 6;
+bool showImage = false;
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({super.key});
-  int a = 0;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-List<String> networkImages = [
+List<String> listulr = [
   "https://haycafe.vn/wp-content/uploads/2023/01/Hinh-anh-thien-nhien-dep-nhat.jpeg",
-  // "https://www.google.com/imgres?imgurl=https%3A%2F%2Fkhoinguonsangtao.vn%2Fwp-content%2Fuploads%2F2023%2F01%2Fhinh-anh-dong-vat-de-thuong.jpg&tbnid=URY0_VnaupCHoM&vet=12ahUKEwiYg4r5se2BAxV91TQHHbYHCN0QMygGegQIARBY..i&imgrefurl=https%3A%2F%2Fkhoinguonsangtao.vn%2Fhinh-anh-dong-vat-de-thuong%2F&docid=VtFQnY1cmC4SPM&w=2564&h=1604&q=%E1%BA%A3nh%20con%20v%E1%BA%ADt%20%C4%91%E1%BA%B9p&ved=2ahUKEwiYg4r5se2BAxV91TQHHbYHCN0QMygGegQIARBY",
   "https://haycafe.vn/wp-content/uploads/2022/04/Hinh-anh-dong-vat-de-thuong-Tho-va-Meo-800x500.jpg",
   "https://haycafe.vn/wp-content/uploads/2022/04/Hinh-anh-dong-vat-de-thuong-cute-800x500.jpg",
   "https://haycafe.vn/wp-content/uploads/2022/04/Hinh-anh-dong-vat-de-thuong-gia-dinh-chim-canh-cut-800x533.jpg",
   "https://haycafe.vn/wp-content/uploads/2022/04/Hinh-anh-dong-vat-de-thuong-cute-2-chu-Tho-800x500.jpg",
   "https://haycafe.vn/wp-content/uploads/2022/04/Hinh-anh-dong-vat-de-thuong-2-chu-meo-con-800x500.jpg",
-  // "https://haycafe.vn/wp-content/uploads/2022/04/Cac-hinh-anh-con-vat-de-thuong-800x600.jpg",
   "https://haycafe.vn/wp-content/uploads/2022/03/Lang-hoa-Sa-Dec-800x535.jpg",
   "https://haycafe.vn/wp-content/uploads/2022/03/anh-danh-lam-thang-canh-Lang-hoa-Sa-Dec-800x450.jpg",
-  //"https://haycafe.vn/wp-content/uploads/2022/03/Lang-hoa-Sa-Dec-khu-du-lich-800x450.jpg",
 ];
 
-List<ImageBox> l1 = [
-  ImageBox(),
-  ImageBox(),
-  ImageBox(),
-  ImageBox(),
-  ImageBox(),
-  ImageBox()
-];
-List<ImageBox> imageBoxs = [...l1];
+List<String> list1 = ["", "", "", "", "", ""];
+final ValueNotifier<List<String>> items = ValueNotifier<List<String>>(list1);
+final ValueNotifier<bool> _isLoadingMore = ValueNotifier<bool>(false);
 
-class _MyHomePageState extends State<MyHomePage> {
-  void reset() {
-    setState(() {
-      imageBoxs.clear();
-      imageBoxs = [...l1];
-    });
+class _MyAppState extends State<MyApp> {
+  bool _isRefresh = false;
+  void _refresh() {
+    _isLoadingMore.value = false;
+    _isRefresh = true;
+    // setState(() {
+    list1 = ["", "", "", "", "", ""];
+    items.value = list1;
+    //});
   }
 
-  bool imageShow = false;
+  Future<void> _loadMoreData() async {
+    if (_isRefresh) {
+      _isRefresh = false;
+    } else {
+      // setState(() {
+      _isLoadingMore.value = true;
+      //});
 
-  String assetImage = "";
+      await Future.delayed(const Duration(seconds: 2));
+      setState(() {
+        list1.addAll(List.generate(6, (index) => ""));
+        items.value = list1;
+      });
 
-  // ignore: unused_field
-  final double _circular = 20;
+      _isLoadingMore.value = false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Card"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(20),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20,
-              ),
-              itemCount: imageBoxs.length,
-              itemBuilder: (context, index) {
-                // print("v1");
-                // setState(() {
-                // });
-                return imageBoxs[index];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Card"),
+          actions: [
+            InkWell(
+                onTap: () {
+                  _refresh();
+                },
+                child: const Icon(Icons.refresh))
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RefreshIndicator(
+            onRefresh: () async {
+              // print("abc");
+              _refresh();
+            },
+            child: NotificationListener(
+              onNotification: (notification) {
+                if (notification is ScrollEndNotification) {
+                  _loadMoreData();
+                }
+
+                return false;
               },
+              child: ValueListenableBuilder(
+                valueListenable: items,
+                builder: (BuildContext context, value, Widget? child) {
+                  return ValueListenableBuilder(
+                    valueListenable: _isLoadingMore,
+                    builder: (BuildContext context, value, Widget? child) {
+                      return GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 10,
+                          crossAxisSpacing: 10,
+                        ),
+                        itemCount: (_isLoadingMore.value)
+                            ? items.value.length + 1
+                            : items.value.length,
+                        itemBuilder: (context, index) {
+                          if (index < items.value.length) {
+                            return Card(index: index);
+                          } else if (_isLoadingMore.value) {
+                            return const CircularProgressIndicator();
+                          }
+                          return null;
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.all(30),
-            width: double.infinity,
-            color: Colors.blue,
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  //  imageBoxs.clear();
-                  //hideAll = true;
-                  // ImageBox i1 = ImageBox();
-
-                  // imageBoxs.add(i1);
-                  print("v");
-                  imageBoxs.clear();
-                  imageBoxs = [...l1];
-                });
-
-                //_resetAll = false;
-                //print(_resetAll);
-              },
-              child: const Text(
-                "Reset",
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 }
 
-class ImageBox extends StatefulWidget {
-  ImageBox({super.key, this.imageShow = false, this.networkImage = ""});
-
-  bool imageShow;
-  String networkImage;
+class Card extends StatefulWidget {
+  Card({super.key, required this.index});
+  int index;
 
   @override
-  State<ImageBox> createState() => _ImageBoxState();
+  State<Card> createState() => _CardState();
 }
 
-class _ImageBoxState extends State<ImageBox> {
-  _ImageBoxState();
-
-  final double _circular = 20;
-
+class _CardState extends State<Card> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         color: Colors.blue,
-        borderRadius: BorderRadius.all(Radius.circular(_circular)),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
       ),
-      child: (!hideAll && !widget.imageShow)
+      child: (items.value[widget.index].isEmpty)
           ? InkWell(
               onTap: () {
-                //print(Random().nextInt(10));
                 setState(() {
-                  hideAll = false;
-                  widget.imageShow = !widget.imageShow;
-                  widget.networkImage =
-                      networkImages[Random().nextInt(networkImages.length)];
-
-                  print(widget.networkImage);
+                  items.value[widget.index] =
+                      listulr[Random().nextInt(listulr.length)];
+                  //showImage = true;
                 });
               },
               child: const Icon(
@@ -167,12 +158,11 @@ class _ImageBoxState extends State<ImageBox> {
               ),
             )
           : ClipRRect(
-              borderRadius: BorderRadius.circular(_circular),
+              borderRadius: BorderRadius.circular(20),
               // clipBehavior: Clip.antiAlias,
               child: Image(
-                  fit: BoxFit.cover, image: NetworkImage(widget.networkImage))),
+                  fit: BoxFit.cover,
+                  image: NetworkImage(items.value[widget.index]))),
     );
   }
 }
-
-bool hideAll = false;
