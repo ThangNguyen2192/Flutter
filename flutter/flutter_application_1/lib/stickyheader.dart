20efef3bd74adfaaad0e78330c8dfea280f414ec
ThangNguyen2192 <nguyenthang.effect@gmail.com>
2023-10-13 16:31:42 +0700

    abc

--- flutter/flutter_application_1/lib/stickyheader.dart
+++ flutter/flutter_application_1/lib/stickyheader.dart
@@ -13,19 +13,23 @@ class MyWidget extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: const CustomScrollView(
+        body: CustomScrollView(
           slivers: [
             CustomSliverStickyHeader(
               title: "Header0",
+              backgroundColor: Colors.blue,
             ),
             CustomSliverStickyHeader(
               title: "Header1",
+              backgroundColor: Colors.yellow,
             ),
             CustomSliverStickyHeaderBuilder(
               title: "Header builder 1",
+              backgroundColor: Colors.pink,
             ),
             CustomSliverStickyHeaderBuilder(
               title: "Header builder 2",
+              backgroundColor: Colors.green,
             ),
           ],
         ),
@@ -35,8 +39,10 @@ class MyWidget extends StatelessWidget {
 }
 
 class CustomSliverStickyHeaderBuilder extends StatefulWidget {
-  const CustomSliverStickyHeaderBuilder({super.key, required this.title});
+  CustomSliverStickyHeaderBuilder(
+      {super.key, required this.title, this.backgroundColor = Colors.blue});
   final String title;
+  Color backgroundColor;
   @override
   State<CustomSliverStickyHeaderBuilder> createState() =>
       _CustomSliverStickyHeaderBuilderState();
@@ -45,18 +51,19 @@ class CustomSliverStickyHeaderBuilder extends StatefulWidget {
 class _CustomSliverStickyHeaderBuilderState
     extends State<CustomSliverStickyHeaderBuilder> {
   int _childCount = 20;
+  Icon icon = const Icon(Icons.arrow_upward);
   @override
   Widget build(BuildContext context) {
     return SliverStickyHeader.builder(
       builder: (context, state) => Container(
         //height: 60.0,
-        color: (state.isPinned ? Colors.pink : Colors.lightBlue)
-            .withOpacity(1.0 - state.scrollPercentage),
+        // color: (state.isPinned ? Colors.pink : Colors.lightBlue)
+        //     .withOpacity(1.0 - state.scrollPercentage),
         // padding: const EdgeInsets.symmetric(horizontal: 16.0),
         alignment: Alignment.centerLeft,
         child: Container(
           height: 60.0,
-          color: Colors.blue,
+          color: widget.backgroundColor,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -64,15 +71,16 @@ class _CustomSliverStickyHeaderBuilderState
               InkWell(
                   onTap: () {
                     setState(() {
-                      print("a");
                       if (_childCount == 20) {
                         _childCount = 0;
+                        icon = const Icon(Icons.arrow_downward);
                       } else {
                         _childCount = 20;
+                        icon = const Icon(Icons.arrow_upward);
                       }
                     });
                   },
-                  child: const Icon(Icons.arrow_downward)),
+                  child: icon),
             ],
           ),
         ),
@@ -96,8 +104,10 @@ class _CustomSliverStickyHeaderBuilderState
 }
 
 class CustomSliverStickyHeader extends StatefulWidget {
-  const CustomSliverStickyHeader({super.key, required this.title});
+  CustomSliverStickyHeader(
+      {super.key, required this.title, this.backgroundColor = Colors.blue});
   final String title;
+  Color backgroundColor;
   @override
   State<CustomSliverStickyHeader> createState() =>
       _CustomSliverStickyHeaderState();
@@ -105,13 +115,14 @@ class CustomSliverStickyHeader extends StatefulWidget {
 
 class _CustomSliverStickyHeaderState extends State<CustomSliverStickyHeader> {
   int _childCount = 20;
+  Icon icon = const Icon(Icons.arrow_upward);
 
   @override
   Widget build(BuildContext context) {
     return SliverStickyHeader(
       header: Container(
         height: 50.0,
-        color: Colors.blue,
+        color: widget.backgroundColor,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -121,12 +132,14 @@ class _CustomSliverStickyHeaderState extends State<CustomSliverStickyHeader> {
                   setState(() {
                     if (_childCount == 20) {
                       _childCount = 0;
+                      icon = const Icon(Icons.arrow_downward);
                     } else {
                       _childCount = 20;
+                      icon = const Icon(Icons.arrow_upward);
                     }
                   });
                 },
-                child: const Icon(Icons.arrow_downward)),
+                child: icon),
           ],
         ),
       ),
